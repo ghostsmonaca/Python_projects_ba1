dce1648bf96dea933cc9296543f7228b51cf4fc7
Mika <kina.broeders@telenet.be>
2024-09-17 10:55:08 +0200

    added info

--- test.py
+++ test.py
@@ -5,3 +5,21 @@ print('mango\'s')
 # modulo operator produceert de rest
 # van een deling -> %
 
+# een variabele is een plaats in
+# het geheugen waar je een naam
+# aan kan geven
+
+variabele_naam = "dit is een variabele"
+
+#f-strings
+x = 2
+y = 3
+print(f"x = {x}")
+#x = 2
+# f string geeft de waarde mee in
+# een string
+
+# type waarde opvragen
+print(type(x))
+#<class 'int'>
+
